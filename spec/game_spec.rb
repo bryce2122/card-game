d7c7774c5dc141316173af8b9513f194fb566888
Samuel Williams <samuelwilliams@samuels-air.lan>
2022-02-08 16:35:27 -0600

    refact spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,15 +6,14 @@ require_relative '../lib/player'
 describe Game do
 
   let(:deck) { Deck.new }
+  let(:player_1) { Player.new("Tom") }
+  let(:player_2) { Player.new("Harry") }
+  let(:player_3) { Player.new("Ron") }
+  let(:player_4) { Player.new("Sirius") }
+  let(:player_5) { Player.new("Ginny") }
+  subject {Game.new(deck, [player_1, player_2,player_3,player_4,player_5]) }
 
   describe 'Starting the game' do
-
-    let(:player_1) { double(:player, name: "Joffrey") }
-    let(:player_2) { double(:player, name: "Tywin") }
-    let(:player_3) { double(:player, name: "Cersei") }
-    let(:player_4) { double(:player, name: "Jaime") }
-    let(:player_5) { double(:player, name: "Tyrion") }
-    subject {Game.new(deck, [player_1, player_2,player_3,player_4,player_5]) }
 
     it "initializes with an array of players" do
       puts subject.players.count
@@ -22,16 +21,7 @@ describe Game do
     end
   end
 
-
   describe 'scoring and finishing the game' do
-
-    #Needs refactoring to use doubles
-    let(:player_1) { Player.new("Tom") }
-    let(:player_2) { Player.new("Harry") }
-    let(:player_3) { Player.new("Ron") }
-    let(:player_4) { Player.new("Sirius") }
-    let(:player_5) { Player.new("Ginny") }
-    subject {Game.new(deck, [player_1, player_2,player_3,player_4,player_5]) }
 
     it "checks the rank of the card and returns 13 if King" do
       expect(subject.convert_value("King")).to eq 13
